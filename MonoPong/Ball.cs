597a0a08e931b3b1b378287b5677d63df46927cc
Eric Morse <36180341+aidkvndfj@users.noreply.github.com>
2021-03-12 19:56:25 -0500

    Initial Commit

---
+++ MonoPong/Ball.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoPong {
+    class Ball {
+        // Public Variabels
+        public Texture2D ballTexture { get; set; }
+
+        // Private Varibales
+        private Vector2 velocity;
+        private Rectangle ballRect;
+        private float speed;
+
+        // Public Objects
+
+        // Private Objects
+        GraphicsDeviceManager graphics;
+        Random random;
+
+        public Ball(Texture2D texture, Vector2 initialPos, GraphicsDeviceManager _graphics) {
+            graphics = _graphics;
+            ballTexture = texture;
+
+            speed = 7f;
+
+            random = new Random();
+            ballRect = new Rectangle((int)initialPos.X, (int)initialPos.Y, 20, 20);
+            velocity = new Vector2(speed, speed);
+        }
+
+        public void Update(GameTime gameTime, Rectangle paddle1, Rectangle paddle2) {
+            var kstate = Keyboard.GetState();
+
+            if (ballRect.Y < 0 || ballRect.Y + ballRect.Height > graphics.PreferredBackBufferHeight) {
+                velocity.Y *= -1;
+            }
+
+            if (ballRect.Intersects(paddle1) || ballRect.Intersects(paddle2)) {
+                velocity.X *= -1;
+                velocity.Y += random.Next(-3, 3);
+                if (velocity.Y < 3 && velocity.Y > 0)
+                    velocity.Y = speed;
+                if (velocity.Y > -3 && velocity.Y <= 0)
+                    velocity.Y = -speed;
+
+                if (velocity.Y > 10)
+                    velocity.Y = speed;
+                if (velocity.Y < -10)
+                    velocity.Y = -speed;
+            }
+
+            ballRect.Y += (int)velocity.Y;
+            ballRect.X += (int)velocity.X;
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Begin();
+            spriteBatch.Draw(ballTexture, ballRect, Color.White);
+            spriteBatch.End();
+        }
+
+        public int OutOfBounds() {
+            if (ballRect.X < 0) {
+                return 1;
+            } else if (ballRect.X > graphics.PreferredBackBufferWidth) {
+                return 2;
+            }
+            return 0;
+        }
+
+        public int GetYPos() {
+            return ballRect.Y + ballRect.Height / 2;
+        }
+
+        public void Reset() {
+            ballRect.X = graphics.PreferredBackBufferWidth / 2;
+            ballRect.Y = graphics.PreferredBackBufferHeight / 2;
+            velocity.X *= -1;
+            velocity.Y += random.Next(-1, 1);
+        }
+    }
+}
---
+++ MonoPong/Game1.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoPong {
+    public class Game1 : Game {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D paddleTexture;
+        private Texture2D ballTexture;
+        private SpriteFont scoreFont;
+        private Paddle player1;
+        private Paddle player2;
+        private Ball ball;
+
+        private int player1Score = 0;
+        private int player2Score = 0;
+
+        public Game1() {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize() {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Window.Title = "MonoPong";
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
+
+            // TODO: use this.Content to load your game content here
+            paddleTexture = Content.Load<Texture2D>("paddle");
+            ballTexture = Content.Load<Texture2D>("ball");
+            scoreFont = Content.Load<SpriteFont>("Score");
+
+            player1 = new Paddle(paddleTexture, new Vector2(50, _graphics.PreferredBackBufferHeight / 2), _graphics, false);
+            player2 = new Paddle(paddleTexture, new Vector2(_graphics.PreferredBackBufferWidth - 50 - paddleTexture.Width, _graphics.PreferredBackBufferHeight / 2), _graphics, true);
+            ball = new Ball(ballTexture, new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2), _graphics);
+        }
+
+        protected override void Update(GameTime gameTime) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            player1.Update(gameTime);
+            player2.Update(gameTime, ball.GetYPos());
+            //player2.Update(gameTime);
+            ball.Update(gameTime, player1.GetRact(), player2.GetRact());
+
+            if (ball.OutOfBounds() == 1) {
+                player2Score += 1;
+                ball.Reset();
+            }
+            if (ball.OutOfBounds() == 2) {
+                player1Score += 1;
+                ball.Reset();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            ball.Draw(_spriteBatch);
+            player1.Draw(_spriteBatch);
+            player2.Draw(_spriteBatch);
+
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(scoreFont, "P1: " + player1Score, new Vector2(10, 10), Color.Black);
+            _spriteBatch.DrawString(scoreFont, "P2: " + player2Score, new Vector2(_graphics.PreferredBackBufferWidth - 50, 10), Color.Black);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
